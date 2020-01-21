c85c7f37b28cea5f5181df8c4d26246e7dcc3385
sunzhiwei1 <sunzhiwei1@xiaomi.com>
2020-01-21 16:36:53 +0800

    增加　go　的消费者

---
+++ demo-thrift-go-comsumer/src/main/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"demo-thrift-client/com/sinbad/thrift/demo"
+	"fmt"
+	"github.com/apache/thrift/lib/go/thrift"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+
+	fmt.Println("Start ....")
+
+	tBufferedTransportFactory := thrift.NewTBufferedTransportFactory(8192)
+	newTSocket, err := thrift.NewTSocket(net.JoinHostPort("127.0.0.1", "8089"))
+	if err != nil {
+		fmt.Print("Error to create socket, err=", err)
+		os.Exit(1)
+	}
+
+	tTransport, err := tBufferedTransportFactory.GetTransport(newTSocket)
+
+	if err:= newTSocket.Open(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error open socket to ...",  err)
+		os.Exit(1)
+	}
+
+	protocolTransport := thrift.NewTBinaryProtocolFactoryDefault()
+	serviceClientFactory := demo.NewHelloWorldServiceClientFactory(tTransport, protocolTransport)
+	defer newTSocket.Close()
+
+	nowTime := time.Time{}
+	println(time.Now().Format("2006-01-02 15:04"))
+	fmt.Print(nowTime)
+
+	cancelContext, cancelFunc := context.WithDeadline(context.Background(), nowTime)
+
+	sayHello, err := serviceClientFactory.SayHello(cancelContext ,"Sinbad")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Client Say Hello failed, err = ",  err)
+		os.Exit(1)
+	}
+
+	deadline, autoCancel := cancelContext.Deadline()
+	//非自动取消
+	if !autoCancel {
+		//取消执行
+		cancelFunc()
+		fmt.Println("autoCancel ")
+	} else {
+		fmt.Println(deadline)
+	}
+
+
+	fmt.Print("Say hello result : ", sayHello)
+
+
+
+}
